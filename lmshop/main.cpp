a8ea248ccd5825ffa0f0b16550392a318a6f7377
Dmitry Starushko <dmitry.starushko@gmail.com>
2023-12-30 17:56:23 +0300

    org 786 code cleanup

--- lmshop/main.cpp
+++ lmshop/main.cpp
@@ -1,14 +1,8 @@
 #include "lmshopapp.h"
 #include "wthelpers.h"
-#include "tools.h"
 
 #include <format>
-#include <list>
 #include <memory>
-#include <map>
-#include <string>
-#include <tuple>
-#include <thread>
 #include <Wt/WServer.h>
 #include <Wt/WResource.h>
 
@@ -17,23 +11,18 @@ int main(int argc, char *argv[]) {
     using namespace Wt;
     using namespace string_literals;
 
-    //    Tools::quota(4294967296, 5368709120);
-    //    while (true) {
     try {
         if (auto server = init_server<LmShopApp>(argc, argv); server->start()) {
             add_embeddables<LmShopApp>(server.get(), "/lmshop");
             auto signal = WServer::waitForShutdown();
             cerr << format("Shutdown (signal = {})\n", signal);
             server->stop();
-            //            break;
         }
     } catch (WServer::Exception &e) {
         cerr << format("Server exception: {}\n", e.what());
     } catch (exception &e) {
         cerr << format("Generic exception: {}\n", e.what());
     } catch (...) { }
-
-    //    }
 
     return 0;
 }
