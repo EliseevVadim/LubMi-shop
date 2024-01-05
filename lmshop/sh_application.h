6cdfd8b8263753e05df77093ed4048e3a921edac
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-01-05 18:05:07 +0300

    org 787 done

--- lmshop/sh_application.h
+++ lmshop/sh_application.h
@@ -15,6 +15,17 @@ class ShopApplication : public EmbeddableApp {
     std::string title() const override;
 
   private:
+    std::unique_ptr<Wt::WContainerWidget> createHeader();
+    std::unique_ptr<Wt::WContainerWidget> createFooter();
+    std::unique_ptr<Wt::WContainerWidget> createMainPage();
+    std::unique_ptr<Wt::WContainerWidget> createCataloguePage();
+    std::unique_ptr<Wt::WContainerWidget> createPerfumeryPage();
+    std::unique_ptr<Wt::WContainerWidget> createDeliveryPage();
+    std::unique_ptr<Wt::WContainerWidget> createCarePage();
+    std::unique_ptr<Wt::WContainerWidget> createContactsPage();
+    std::unique_ptr<Wt::WContainerWidget> createAboutCompanyPage();
+    std::unique_ptr<Wt::WContainerWidget> createUnderConstructionPage(const std::string &page_title);
+
     std::list<Wt::Signals::connection> connections_;
     static Wt::Signal<const ShopApplication *, const std::string &> broadcast_message_; //TODO map of string -> signal
     PersistData _persist_data;
