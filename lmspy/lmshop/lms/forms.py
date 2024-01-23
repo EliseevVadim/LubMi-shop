80bf3ab125fdc8b693fd6b92373459e97c604332
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-01-24 02:06:27 +0300

    org 801: in progress

--- lmspy/lmshop/lms/forms.py
+++ lmspy/lmshop/lms/forms.py
@@ -3,7 +3,20 @@ from .models import CustomerShortInfo
 
 
 class CustomerShortInfoForm(forms.ModelForm):
+    title = 'Сообщить о поступлении товара'
+    description = 'Выберите удобный способ для оповещения о повторном наличии данного товара'
+    confirmation = "Получить уведомление"
+
     class Meta:
         model = CustomerShortInfo
         fields = "__all__"
-        labels = {"name": "Имя", "phone": "Телефон", "email": "E-mail"}
+        labels = {
+            "name": '',
+            "phone": '',
+            "email": '',
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Как к Вам обращаться'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Телефон'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Почта'}),
+        }
