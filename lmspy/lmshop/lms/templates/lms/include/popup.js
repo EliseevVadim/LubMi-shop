2e845dd872fac1717c76ed63f8eac45a8153b495
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-01-25 01:40:01 +0300

    org 802: done

---
+++ lmspy/lmshop/lms/templates/lms/include/popup.js
@@ -0,0 +1,7 @@
+const show_popup = (text) => {
+  document.querySelector("#{{popup_id}} span").textContent = text;
+  document.getElementById('{{popup_id}}').style.visibility = "visible";
+};
+const hide_popup = () => {
+  document.getElementById('{{popup_id}}').style.visibility = "hidden";
+};
