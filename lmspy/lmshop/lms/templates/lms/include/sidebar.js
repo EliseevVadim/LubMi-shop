48b620e6677eb9b179099cf6fb5b5f522c263b79
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-01-28 03:43:57 +0300

    org 806: in progress

--- lmspy/lmshop/lms/templates/lms/include/sidebar.js
+++ lmspy/lmshop/lms/templates/lms/include/sidebar.js
@@ -1,6 +1,3 @@
-const hide_{{side}}_sidebar = () => {
-    document.getElementById('_{{side}}_sidebar').style.visibility = "hidden";
-};
 const {{side}}_sidebar = {
     self: () => { return document.getElementById('_{{side}}_sidebar'); },
     show: url => { fetch(url).then(response => response.text()).then(html => {
