90cec12aad61ca9cc72c042e611fbdcb2e81a320
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-02-02 00:54:30 +0300

    org 811: in progress

--- lmspy/lmshop/lms/templates/lms/include/c6t-dialog-ext.js
+++ lmspy/lmshop/lms/templates/lms/include/c6t-dialog-ext.js
@@ -18,8 +18,55 @@ c6t_dialog.show = () => {
             c6t_dialog.sidebar().innerHTML = html;
         });
         window.addEventListener(EventType.SCART_CHANGED, c6t_dialog.__on_scart_changed);
-        // TODO prefill fields from CI
-        c6t_dialog.self().showModal();
-        scart_changed();
+        __api_call__('{% url "api:get_customer_info" flags=15 %}', null, answer => {
+            let find = id => document.getElementById(id);
+
+            _form = find('c6t-form');
+            _name = find('c6t-cu_name');
+            _phone = find('c6t-cu_phone');
+            _email = find('c6t-cu_email');
+            _city = find('c6t-cu_city');
+            _street = find('c6t-cu_street');
+            _building = find('c6t-cu_building');
+            _entrance = find('c6t-cu_entrance');
+            _floor = find('c6t-cu_floor');
+            _apartment = find('c6t-cu_apartment');
+            _fullname = find('c6t-cu_fullname');
+            _confirm = find('c6t-cu_confirm');
+            _d7y_service_0 = find('c6t-d7y_service_0');
+            _d7y_service_1 = find('c6t-d7y_service_1');
+
+            _name.value = answer.name;
+            _phone.value = answer.phone;
+            _email.value = answer.email;
+            _city.value = answer.address.city;
+            _street.value = answer.address.street;
+            _building.value = answer.address.building;
+            _entrance.value = answer.address.entrance;
+            _floor.value = answer.address.floor;
+            _apartment.value = answer.address.apartment;
+            _fullname.value = answer.address.fullname;
+
+            _email.oninput = _ => { if(_email.value) _phone.removeAttribute('required'); else _phone.setAttribute('required',''); }
+            _phone.oninput = _ => { if(_phone.value) _email.removeAttribute('required'); else _email.setAttribute('required',''); }
+
+            _form.onsubmit = e => {
+                e.preventDefault();
+                __api_call__('{% url "api:c6t" %}', {
+                    cu_name: _name.value,
+                    cu_phone: _phone.value,
+                    cu_email: _email.value
+                }, result => {
+                    if(result.success) {
+                    } else {
+                    }
+                });
+            }
+            _email.oninput(null);
+            _phone.oninput(null);
+
+            c6t_dialog.self().showModal();
+            scart_changed();
+        });
     });
 };
