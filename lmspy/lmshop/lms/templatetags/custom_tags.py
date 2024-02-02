b7038b6b21e070b08b9971772d3d538cc30ba1eb
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-02-02 19:28:52 +0300

    org 816: in progress

--- lmspy/lmshop/lms/templatetags/custom_tags.py
+++ lmspy/lmshop/lms/templatetags/custom_tags.py
@@ -1,24 +1,9 @@
 from django import template
+from django.utils.safestring import mark_safe
+import markdown
 register = template.Library()
 
 
-class VariableNode(template.Node):
-    def __init__(self, var_name, var_value):
-        self.var_name = var_name
-        self.var_value = var_value
-
-    def render(self, context):
-        try:
-            value = template.Variable(self.var_value).resolve(context)
-        except template.VariableDoesNotExist:
-            value = ""
-        context[self.var_name] = value
-        return ""
-
-
-@register.tag(name='set')
-def set_variable(_, token):
-    parts = token.split_contents()
-    if len(parts) != 4:
-        raise template.TemplateSyntaxError("'set' tag must be of the form: {% set <var_name> = <var_value> %}")
-    return VariableNode(parts[1], parts[3])
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
