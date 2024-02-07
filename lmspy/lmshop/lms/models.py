641257aa2466f297d00d36fe073c6ebef23f2d9e
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-02-07 17:01:19 +0300

    org 813: in progress

--- lmspy/lmshop/lms/models.py
+++ lmspy/lmshop/lms/models.py
@@ -284,7 +284,7 @@ class Chat(DbItem):
         return f'Телеграм-чат "{self.title}"'
 
 
-class Collaborator(DbItem):
+class Coworker(DbItem):
     key = models.CharField(primary_key=True, max_length=2)                                      # -- ключ --
     title = models.CharField(max_length=50)                                                     # -- название --
     description = models.TextField(null=True, blank=True)                                       # -- описание --
@@ -296,8 +296,8 @@ class Collaborator(DbItem):
     @staticmethod
     def value_of(cpk, spk, default=None):
         try:
-            return Collaborator.objects.get(pk=cpk).settings.get(pk=spk).value
-        except Collaborator.DoesNotExist:
+            return Coworker.objects.get(pk=cpk).settings.get(pk=spk).value
+        except Coworker.DoesNotExist:
             return default
         except Setting.DoesNotExist:
             return default
@@ -307,7 +307,7 @@ class Setting(DbItem):
     key = models.CharField(primary_key=True, max_length=32)                                     # -- ключ --
     value = models.CharField(max_length=150)                                                    # -- значение --
     description = models.TextField(null=True, blank=True)                                       # -- описание --
-    owner = models.ForeignKey(Collaborator, related_name="settings", on_delete=models.CASCADE)  # -- владелец --
+    owner = models.ForeignKey(Coworker, related_name="settings", on_delete=models.CASCADE)  # -- владелец --
 
     def __str__(self):
         return f'<{self.owner.key}>:<{self.key}>:<{self.value}>'
