4599f0edf8171ed307f766e8c7c925be8156a890
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-02-14 11:17:38 +0300

    org-814: done

--- lmspy/lmshop/lms/templates/lms/include/search.js
+++ lmspy/lmshop/lms/templates/lms/include/search.js
@@ -1,13 +1,29 @@
 const sch = {
-    box: () => document.querySelector('#search-box'),
-    text: () => document.querySelector('#search-text'),
-    result: () => document.querySelector('#search-result'),
+    dialog: () => document.querySelector('#sch-dialog'),
+    box: () => document.querySelector('#sch-box'),
+    text: () => document.querySelector('#sch-text'),
+    result: () => document.querySelector('#sch-result'),
+    footer: () => document.querySelector('#sch-footer'),
     open: () => {
+        sch.dialog().classList.remove('full-screen');
         sch_dialog.show('{% url "lms:search" %}');
     },
+    expand: () => {
+        sch.footer().innerHTML = '<p>Кнопки</p>';
+        sch.dialog().classList.add('full-screen');
+    },
+    collapse: () => {
+        sch.footer().innerHTML = '';
+        sch.dialog().classList.remove('full-screen');
+    },
+    tid: null,
     update: (text) => {
-        fetch('{% url "lms:index" %}'+'?page=1').then(response => response.text()).then(html => {
-            sch.result().innerHTML = html;
-        });
+        if(sch.tid) { clearTimeout(sch.tid) }
+        sch.tid = setTimeout(_ => {
+            (text ? sch.expand : sch.collapse)();
+            fetch('{% url "lms:index" %}'+'?page=1').then(response => response.text()).then(html => {
+                sch.result().innerHTML = html;
+            });
+        }, text ? 1000 : 100);
     },
 };
