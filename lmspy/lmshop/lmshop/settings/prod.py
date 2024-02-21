85d44f99fcb1a2b49c241a05ac0e93f002b6d1d7
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-02-21 22:21:45 +0300

    org-836 in progress

--- lmspy/lmshop/lmshop/settings/prod.py
+++ lmspy/lmshop/lmshop/settings/prod.py
@@ -1,6 +1,15 @@
 from .base import *
+import os
 
 DEBUG = False
 ADMINS = [('Dmitry', 'dmitry.starushko@gmail.com')]
 ALLOWED_HOSTS = ['*']
-DATABASES = {'default': {}}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }}
