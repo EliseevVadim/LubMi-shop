68b1fe14ea07fe82bff3f049b8c96fafe5c6f411
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-02-22 03:15:46 +0300

    org-836 in progress

--- lmspy/install.sh
+++ lmspy/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker-compose -d up
-docker-compose exec web python manage.py migrate --skip-checks
-docker-compose exec web python manage.py loaddata lms.json
-docker-compose exec web python manage.py createsuperuser
+docker-compose up -d
+docker-compose exec web 'python manage.py migrate --skip-checks'
+docker-compose exec web 'python manage.py loaddata lms.json'
+docker-compose exec web 'python manage.py createsuperuser'
