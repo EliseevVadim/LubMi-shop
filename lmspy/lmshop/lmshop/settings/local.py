4d4b3546302ed7cc58b3dc76d9e4e8b84bb6c3d5
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-02-22 10:44:19 +0300

    org-836 in progress

--- lmspy/lmshop/lmshop/settings/local.py
+++ lmspy/lmshop/lmshop/settings/local.py
@@ -1,8 +1,18 @@
 from .base import *
+import os
 DEBUG = True
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'lmshop.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'lmshop.sqlite3',
-    }
-}
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }}
