4c1e42b1e7cc05eceb601f39028cb53ab3f8490a
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-03-06 15:58:36 +0300

    org-839 in progress

--- lmspy/lmshop/lmshop/settings/prod.py
+++ lmspy/lmshop/lmshop/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 import os
 
-DEBUG = False
+DEBUG = True
 ADMINS = [('Dmitry', 'dmitry.starushko@gmail.com')]
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://localhost/*']
