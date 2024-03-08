7bd1e81b8a1158d601ccd46cf1da44508910bf76
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-03-08 19:29:25 +0300

    org-839 in progress

--- lmspy/lmshop/lms/templates/lms/include/selector.js
+++ lmspy/lmshop/lms/templates/lms/include/selector.js
@@ -1,27 +1,29 @@
-const customSelect = document.querySelector(".custom-select");
-const selectBtn = document.querySelector(".select-button");
-const selectedValue = document.querySelector(".selected-value");
-const optionsList = document.querySelectorAll(".select-dropdown li");
+const {{unique}}_selector = {
+    custom_select: () => document.querySelector("#{{unique}}-custom-select"),
+    select_btn: () => document.querySelector("#{{unique}}-select-button"),
+    selected_value: () => document.querySelector("#{{unique}}-selected-value"),
+    options_list: () => document.querySelectorAll("#{{unique}}-select-dropdown li"),
+    init: () => {
+        {{unique}}_selector.select_btn().addEventListener("click", () => {
+            {{unique}}_selector.custom_select().classList.toggle("active");
+            {{unique}}_selector.select_btn().setAttribute("aria-expanded", {{unique}}_selector.select_btn().getAttribute("aria-expanded") === "true" ? "false" : "true");
+        });
 
-selectBtn.addEventListener("click", () => {
-    customSelect.classList.toggle("active");
-    selectBtn.setAttribute(
-        "aria-expanded",
-        selectBtn.getAttribute("aria-expanded") === "true" ? "false" : "true");
-});
-
-optionsList.forEach((option) => {
-    function handler(e) {
-        if (e.type === "click" && e.clientX !== 0 && e.clientY !== 0) {
-            selectedValue.textContent = this.children[1].textContent;
-            selectedValue.textContent = this.children[0].name;
-            customSelect.classList.remove("active");
-        }
-        if (e.key === "Enter") {
-            selectedValue.textContent = this.textContent;
-            customSelect.classList.remove("active");
-        }
+        {{unique}}_selector.options_list().forEach((option) => {
+            function handler(e) {
+                if (e.type === "click" && e.clientX !== 0 && e.clientY !== 0) {
+                    {{unique}}_selector.selected_value().textContent = this.children[1].textContent;
+                    {{unique}}_selector.selected_value().textContent = this.children[0].name;
+                    {{unique}}_selector.custom_select().classList.remove("active");
+                }
+                if (e.key === "Enter") {
+                    {{unique}}_selector.selected_value().textContent = this.textContent;
+                    {{unique}}_selector.custom_select().classList.remove("active");
+                }
+            }
+            option.addEventListener("keyup", handler);
+            option.addEventListener("click", handler);
+        });
     }
-    option.addEventListener("keyup", handler);
-    option.addEventListener("click", handler);
-});
+};
+
