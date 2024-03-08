a43011e59cb2e8215be9dd390d60f8064165a59e
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-03-08 18:25:06 +0300

    org-839 in progress

--- lmspy/lmshop/lms/templatetags/custom_tags.py
+++ lmspy/lmshop/lms/templatetags/custom_tags.py
@@ -7,3 +7,10 @@ register = template.Library()
 @register.filter(name='markdown')
 def markdown_format(text):
     return mark_safe(markdown.markdown(text, extensions=['tables', 'def_list']))
+
+
+@register.filter
+def first_val_in_dict(dictionary: dict):
+    for key in dictionary.keys():
+        return dictionary[key]
+    return None
