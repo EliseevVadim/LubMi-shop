b9f2154e8a8bbf221613d1aea7562c64a642a57d
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-03-11 19:19:57 +0300

    org-839 in progress

--- lmspy/lmshop/lms/templates/lms/include/popup.js
+++ lmspy/lmshop/lms/templates/lms/include/popup.js
@@ -1,10 +1,23 @@
 const popup = {
     tmid: null,
     self: () => document.getElementById('{{popup_id}}'),
+    free: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    tmids: [null, null, null, null, null, null, null, null, null, null],
     show: text => {
-        if(popup.tmid) { clearTimeout(popup.tmid); }
-        popup.tmid = setTimeout(()=>{ popup.tmid = null; popup.hide(); }, {{param_value_popup_show_time}});
-        document.querySelector("#{{popup_id}} span").textContent = text; popup.self().style.visibility = "visible";
+        var i = popup.free.shift();
+        if(i != undefined) {
+            document.querySelector(`#popup-${i} span`).textContent = text;
+            document.querySelector(`#popup-${i}`).classList.add("active");
+            popup.tmids[i] = setTimeout(function() { popup.hide(i); }, {{param_value_popup_show_time}});
+        }
     },
-    hide: () => { popup.self().style.visibility = "hidden"; },
+    hide: (i) => {
+        if(popup.tmids[i]) {
+            clearTimeout(popup.tmids[i]);
+            popup.tmids[i] = null;
+        }
+        popup.free.push(i);
+        popup.free.sort();
+        document.querySelector(`#popup-${i}`).classList.remove("active");
+    },
 };
