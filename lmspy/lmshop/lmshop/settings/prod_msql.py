712a946bdade1f482ad6889cef6742797fa70655
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-04-15 22:36:53 +0300

    beget deploy

--- lmspy/lmshop/lmshop/settings/prod_msql.py
+++ lmspy/lmshop/lmshop/settings/prod_msql.py
@@ -6,6 +6,7 @@ ADMINS = [('Dmitry', 'dmitry.starushko@gmail.com')]
 SITE_URL = "http://192.168.0.100/lms/"
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://192.168.0.100/*']
+MEDIA_ROOT = BASE_DIR.parent.parent / 'public_html' / 'media'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
