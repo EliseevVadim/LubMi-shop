ab439288529060b92b8a50228fdc2dac6065bea2
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-05-06 23:58:57 +0300

    api

--- lmspy/lmshop/lms/api/middleware.py
+++ lmspy/lmshop/lms/api/middleware.py
@@ -2,5 +2,7 @@ def ACAO(get_response):
     def middleware(request):
         response = get_response(request)
         response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Headers'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = '*'
         return response
     return middleware
