98c1f56b17a97ede9902aeb1a19a208be2e43a23
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-09-04 17:18:05 +0300

    diff cod

--- lmspy/lmshop/lms/api/views.py
+++ lmspy/lmshop/lms/api/views.py
@@ -465,8 +465,7 @@ class Service_EstimateSCart_View(APIView):
             'price': scart['price'],
             'old_price': scart['old_price'],
             'weight': scart['weight'],
-            'errors': errors,
-            'total-price-includes-delivery-cost': not settings.PREFERENCES.CashOnD6y
+            'errors': errors
         }
         if 'cu_city_uuid' in data:
             try:
@@ -480,7 +479,7 @@ class Service_EstimateSCart_View(APIView):
                 else:
                     for ds in D6Y:
                         d6y_cost, d6y_time, error = ds_factory(ds).delivery_cost(city.code, scart["weight"], price=scart['price'])
-                        result[ds] = {'cost': d6y_cost, 'days': d6y_time, 'error': error}
+                        result[ds] = {'cost': d6y_cost, 'days': d6y_time, 'cod': settings.PREFERENCES.c_o_d(ds), 'error': error}
             except (TypeError, KeyError):
                 return Parameter.value_of('message_data_sending_error', 'Произошла ошибка при отправке данных, мы работаем над этим...')
             except ValueError:
