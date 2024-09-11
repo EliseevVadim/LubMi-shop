bd41c4f3c938e2a897748520f756e7a018c1b1ab
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-09-11 23:10:01 +0300

    surcharges

--- lmspy/lmshop/lms/coworkers/surcharges.py
+++ lmspy/lmshop/lms/coworkers/surcharges.py
@@ -2,5 +2,8 @@ from decimal import Decimal
 
 
 def d6y_cost_with_surcharges(d6y_svc: any, d6y_cost: Decimal, goods_cost: Decimal):
-    cost = eval(d6y_svc.setting('delivery-with-surcharges', 'delivery'), {'delivery': float(d6y_cost), 'goods': float(goods_cost)})
+    cost = eval(d6y_svc.setting('delivery-with-surcharges', 'delivery'), {}, {
+        'delivery': float(d6y_cost),
+        'goods': float(goods_cost)
+    })
     return Decimal(round(cost, 2))
