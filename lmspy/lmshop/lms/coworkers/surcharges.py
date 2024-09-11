810b04caf9c07c9afa28ce4ac79f73a987d0a4c0
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-09-11 20:40:48 +0300

    surcharges

--- lmspy/lmshop/lms/coworkers/surcharges.py
+++ lmspy/lmshop/lms/coworkers/surcharges.py
@@ -3,6 +3,6 @@ from decimal import Decimal
 
 def d6y_cost_with_surcharges(d6y_svc: any, d6y_cost: Decimal, goods_cost: Decimal):
     goods_factor = Decimal(d6y_svc.setting("surcharge-goods-percents", "0")) / Decimal(100)
-    d6y_factor = Decimal(1) + Decimal(d6y_svc.setting("surcharge-delivery-percents", "0")) / Decimal(100)
+    agent_factor = Decimal(1) + Decimal(d6y_svc.setting("surcharge-agent-percents", "0")) / Decimal(100)
     add_sur = Decimal(d6y_svc.setting("surcharge-additional", "0"))
-    return round(d6y_cost * d6y_factor + goods_cost * goods_factor + add_sur, 2)
+    return round((d6y_cost + goods_cost * goods_factor) * agent_factor + add_sur, 2)
