6b3a225a046d4801262328271a0efe47bbd69bd4
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-09-18 20:52:27 +0300

    surcharges

--- lmspy/lmshop/lms/coworkers/surcharges.py
+++ lmspy/lmshop/lms/coworkers/surcharges.py
@@ -1,9 +1,15 @@
 from decimal import Decimal
 
 
+# def d6y_cost_with_surcharges(d6y_svc: any, d6y_cost: Decimal, goods_cost: Decimal):
+#     cost = eval(d6y_svc.setting('delivery-with-surcharges', 'delivery'), {}, {
+#         'delivery': float(d6y_cost),
+#         'goods': float(goods_cost)
+#     })
+#     return Decimal(round(cost, 2))
+
 def d6y_cost_with_surcharges(d6y_svc: any, d6y_cost: Decimal, goods_cost: Decimal):
-    cost = eval(d6y_svc.setting('delivery-with-surcharges', 'delivery'), {}, {
-        'delivery': float(d6y_cost),
-        'goods': float(goods_cost)
-    })
-    return Decimal(round(cost, 2))
+    goods_factor = Decimal(d6y_svc.setting("surcharge-goods-percents", "0")) / Decimal(100)
+    agent_factor = Decimal(1) + Decimal(d6y_svc.setting("surcharge-agent-percents", "0")) / Decimal(100)
+    add_sur = Decimal(d6y_svc.setting("surcharge-additional", "0"))
+    return round((d6y_cost + goods_cost * goods_factor) * agent_factor + add_sur, 2)
