52b0c2fe612050d35cf2cc8060e7b97360da45ee
Dmitry Starushko <dmitry.starushko@gmail.com>
2024-09-21 03:09:11 +0300

    stuff actions

--- lmspy/lmshop/lms/urls.py
+++ lmspy/lmshop/lms/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/order/<slug:slug>/', AdminOrderView.as_view(), name='admin_order_details'),
     path('admin/order/<slug:slug>/complete-order/', AdminCompleteOrderView.as_view(), name='admin_complete_order'),
     path('admin/order/<slug:slug>/delivery-documents/', Admin_DeliveryDocuments_View.as_view(), name='admin_order_delivery_documents'),
+    path('admin/action/customers/', Admin_Customers_View.as_view(), name='admin_customers'),
 ]
